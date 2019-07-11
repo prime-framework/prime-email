5ea58a368f75be01bf95cc5c43b9966f59d3e703
Daniel DeGroff <daniel@inversoft.com>
2019-07-11 17:13:17 -0600

    Fix the cause of an NPE, need to pass along the contextId

--- src/main/java/org/primeframework/email/service/DefaultEmailService.java
+++ src/main/java/org/primeframework/email/service/DefaultEmailService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ * Copyright (c) 2001-2019, JCatapult.org, All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -115,7 +115,7 @@ public class DefaultEmailService implements EmailService {
     SendResult sendResult = new SendResult(sendEmailBuilder.getEmail());
     ParsedEmailTemplates parsedEmailTemplates = emailTemplateLoader.load(contextId, templateId, preferredLanguages, sendResult);
     emailRenderer.render(parsedEmailTemplates, sendEmailBuilder.getEmail(), sendEmailBuilder.getParameters(), sendResult);
-    emailTransportService.sendEmailLater(null, sendEmailBuilder.getEmail(), sendResult);
+    emailTransportService.sendEmailLater(contextId, sendEmailBuilder.getEmail(), sendResult);
     return sendResult;
   }
 }
